5c91adf024ac3855c0ad4e964df3e5c97847e091
Youanas Ibrahim <youanasibrahim@gmail.com>
2026-07-19 15:48:27 +0300

    add new file

---
+++ Task-Session-1/Problem_1.js
@@ -0,0 +1,21 @@
+var isValid = function(s) {
+    let stack = [];
+
+    const pairs = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    };
+
+    for (let char of s) {
+        if (char === '(' || char === '[' || char === '{') {
+            stack.push(char);
+        } else {
+            if (stack.pop() !== pairs[char]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
